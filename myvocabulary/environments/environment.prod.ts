b416a51501f351149978a578aa698262a60cc6e3
Sasikumar Rathinam <sasikumarr@vishwak.com>
2017-10-04 14:30:26 +0530

    update

---
+++ myvocabulary/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true
+};
+
+export const firebaseConfig = {
+    apiKey: '',
+    authDomain: 'myvocabularylive.firebaseapp.com',
+    databaseURL: 'https://myvocabularylive.firebaseio.com',
+    storageBucket: 'myvocabularylive.appspot.com'
+}
